d904679cab8c189a88c0a60da2c884846532d709
Jesus Ibarra <phenom133@pop-os.attlocal.net>
2025-04-08 16:27:56 -0500

    finish project

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, character_sort_list
+import sys
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 
 def main():
-    text = get_book_text('books/frankenstein.txt')
-    print(f"{count_words(text)} words found in the document")
-    print(count_characters(text))
+    args = sys.argv
+    if len(args) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = args[1]
+    text = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text)} total words")
+    print("--------- Character Count -------")
+    characters_dictionary = count_characters(text)
+    sorted_character_list = character_sort_list(characters_dictionary)
+    for list_item in sorted_character_list:
+        character = list(list_item.keys())[0]
+        print(f"{character}: {list_item[character]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,12 @@ def count_characters(text):
         character_dict[char] = character_dict.get(char, 0) + 1
 
     return character_dict
+
+def character_sort_list(character_dictionary):
+    character_list = []
+    for character in character_dictionary:
+        if character.isalpha():
+           single_character_dict = {character: character_dictionary.get(character)}
+           character_list.append(single_character_dict)
+
+    return sorted(character_list, key=lambda d: list(d.values())[0],reverse= True)
